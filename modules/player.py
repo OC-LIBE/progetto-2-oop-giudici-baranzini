cd2c5b14f4086b944e2c6ddc1a1e0a3777829645
giudicim <magagilu77@gmail.com>
2025-02-05 07:22:35 +0000

    correzzione rapporto

--- modules/player.py
+++ modules/player.py
@@ -2,12 +2,3 @@ import streamlit as st
 import numpy as np
 from modules.deck import Deck
 from modules.hand import Hand
-
-
-class Player:
-    def __init__(self):
-        self.hand:Hand = Hand()
-        
-
-    def deal(self):
-        self.hand.deal()
