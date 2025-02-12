1e2e98ae313385f1ca828ac18558870a01cdd57a
mbaranzini <milo.baranzini@hotmail.com>
2025-02-12 07:22:00 +0000

    class player

--- modules/player.py
+++ modules/player.py
@@ -1,4 +1,11 @@
-import streamlit as st
-import numpy as np
 from modules.deck import Deck
 from modules.hand import Hand
+
+
+class Player:
+    def __init__(self):
+        self.hand:Hand = Hand()
+        
+
+    def draw_card(self):
+        self.hand.deal()
